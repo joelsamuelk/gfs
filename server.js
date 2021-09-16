3a712bb36b05b7a34ad3b984d2523b9c760eb157
joelsamuelk <joelsamuelk@gmail.com>
2021-09-16 14:13:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req,res){
-  res.sendFile(path.join(__dirname, 'gfs-web-app', 'index.html'))
+app.get('/*', (req,res) => {
+  res.sendFile(`./dist/gfs-web-app/index.html`);
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
