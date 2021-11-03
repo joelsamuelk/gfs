b324805acd6903506c232198f68ae79426519abd
joelsamuelk <joelsamuelk@users.noreply.github.com>
2021-11-03 07:29:42 +0200

    Revert "Cleanup"
    This reverts commit 11a0a5ce8cff93abfbff2d63d1f6bdd4d8fc358d.

--- gfs-cms/config/database.js
+++ gfs-cms/config/database.js
@@ -11,6 +11,9 @@ module.exports = ({ env }) => ({
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "postgres"),
         schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
       },
       options: {},
     },
