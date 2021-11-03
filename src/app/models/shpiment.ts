e873a8443628ff0b642801a823a6bcb9d1bf2258
joelsamuelk <joelsamuelk@users.noreply.github.com>
2021-11-03 07:29:15 +0200

    Revert "Cleanup"
    This reverts commit 3bf1a841e54ed1c12ba9de57c47763af7af42a2f.

--- src/app/models/shpiment.ts
+++ src/app/models/shpiment.ts
@@ -1,11 +1,10 @@
-import { Document } from './document';
-
+import { Document } from "./document";
 export interface Shipment {
-    vessel: string;
-    Port_Of_Load: string;
-    Container_No: string;
-    Departure: string;
-    Arrival: string;
-    Status: string;
-    Documents?: Array<Document> | null;
+    vessel: string
+    Port_Of_Load: string
+    Container_No: string
+    Departure: string
+    Arrival: string
+    Status: string
+    Documents?: Array<Document> | undefined
 }
