564554ec046dd23cd3ef9db440100a1d216e5813
joelsamuelk <joelsamuelk@users.noreply.github.com>
2021-12-23 10:38:39 +0200

    upated strapi & angular code

--- src/app/models/shpiment.ts
+++ src/app/models/shpiment.ts
@@ -1,10 +1,15 @@
-import { Document } from "./document";
+import { Document } from './document';
+
 export interface Shipment {
-    vessel: string
-    Port_Of_Load: string
-    Container_No: string
-    Departure: string
-    Arrival: string
-    Status: string
-    Documents?: Array<Document> | undefined
+  vessel: string;
+  Port_Of_Load: string;
+  Container_No: string;
+  Departure: string;
+  Arrival: string;
+  Status: string;
+  documents?: Array<Document> | undefined;
+  shipping_line: string;
+  suppliers: [];
+  user: {};
+  Container_value: number;
 }
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 import { CookieService } from 'src/app/cookie.service';
 import { User } from 'src/app/models/user.model';
-import { Document } from 'src/app/models/document';
 import { Shipment as Shipment } from 'src/app/models/shpiment';
 import { environment } from 'src/environments/environment';
 
@@ -14,17 +13,17 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor(private cookieService: CookieService) {
-    this.jwtBearer = null;
-   }
+  private gridApi: any;
+  private gridColumnApi: any;
 
   showModal = true;
   jwtBearer: string | null;
   user: User | undefined;
-  Shipments: Array<Shipment> = [];
-  private gridApi: any;
-  private gridColumnApi: any;
+  shipments: Array<Shipment> = [];
+
+  constructor(private cookieService: CookieService) {
+    this.jwtBearer = null;
+  }
 
   columnDefs = [
     { headerName: 'Vessel', field: 'vessel', resizable: true, sortable: true, filter: true },
@@ -33,80 +32,38 @@ export class UserProfileComponent implements OnInit {
     { headerName: 'Departure', field: 'Departure', resizable: true, sortable: true, filter: true },
     { headerName: 'Arrival', field: 'Arrival', resizable: true, sortable: true, filter: true },
     { headerName: 'Status', field: 'Status', resizable: true, sortable: true, filter: true },
+    { headerName: 'Shipping Line', field: 'shipping_line', resizable: true, sortable: true, filter: true },
     {
       headerName: 'Documents',
+      field: 'documents',
       suppressSizeToFit: true,
       resizable: true,
       cellRenderer: (data: any) => {
-          if (data.data.hasOwnProperty('Documents') && data.data.Documents.length > 0) {
-            let returnHtml = '<select style="max-width: 160px;" class="btn btn-sm" onChange="browseToDocument(this)"><option value="0">View document(s)</option>';
+        if (data.data.hasOwnProperty('documents') && data.data.documents.length > 0) {
+          let returnHtml = '<select style="max-width: 160px;" class="btn btn-sm" onChange="browseToDocument(this)"><option value="0">View document(s)</option>';
 
-            data.data.Documents.forEach((element: { url: string; name: string; }) => {
-              returnHtml += `<option value="` + environment.strapiUrl + element.url + `">` + element.name + `</option>`;
-            });
+          data.data.documents.forEach((element: { url: string; name: string; }) => {
+            returnHtml += `<option value="` + environment.strapiUrl + element.url + `">` + element.name + `</option>`;
+          });
 
-            returnHtml += `</select>`;
+          returnHtml += `</select>`;
 
-            return returnHtml;
-          }
-          else{
-            return 'No documents found';
-          }
+          return returnHtml;
+        }
+        else{
+          return 'No documents found';
+        }
       },
       suppressSorting: true
-  }
+    }
   ];
-
   rowData: any;
 
   async ngOnInit(): Promise<any> {
     try{
       this.user = await this.cookieService.verifyUser();
       this.jwtBearer = this.cookieService.readCookie('__jwtBearer');
-
-      const customer = await axios.get(environment.strapiUrl + '/customers?user.username=' + this.user?.username, {
-        headers: {
-          Authorization: `Bearer ${this.jwtBearer}`
-        }
-      });
-
-      const documents = await axios.get(environment.strapiUrl + '/documents?customer.user=' + this.user?.id, {
-        headers: {
-          Authorization: `Bearer ${this.jwtBearer}`,
-        }
-      });
-
-      let userDocuments: any[] = [];
-
-      if (documents.data.length > 0) {
-        userDocuments = documents.data.filter((x: { customer: { user: number | undefined; }; }) => x.customer.user === this.user?.id);
-      }
-
-      if (customer.data[0].shipments != null)
-      {
-        customer.data[0].shipments.forEach((element: any) => {
-          let shipmentDocuments = new Array<Document>();
-
-          if (documents.data.length > 0) {
-            shipmentDocuments = userDocuments.find((x: { shipment: { id: any; }; }) => x.shipment.id === element.id)?.documents;
-          }
-
-          const currentShipment: Shipment = {
-            Arrival: element.Arrival,
-            vessel: element.vessel,
-            Container_No: element.Container_No,
-            Departure: element.Departure,
-            Port_Of_Load: element.Port_Of_Load,
-            Status: element.Status,
-            Documents: shipmentDocuments
-          } as Shipment;
-
-          this.Shipments?.push(currentShipment);
-        });
-      }
-
-      this.rowData = this.Shipments;
-
+      await this.getShipments();
     }
     catch (e){
       console.error(e);
@@ -117,11 +74,47 @@ export class UserProfileComponent implements OnInit {
     params.api.sizeColumnsToFit();
   }
 
+  onExportClicked(): void {
+    this.gridApi.exportDataAsExcel();
+  }
+
   onGridReady(params: { api: { sizeColumnsToFit: () => void; }; columnApi: any; }): void {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
     setTimeout(() => {
       params.api.sizeColumnsToFit();
     }, 500);
+  }
+
+  getShipments(): Promise<Array<Shipment> | void> {
+    return axios
+      .get(environment.strapiUrl + '/shipments?user.username=' + this.user?.username, {
+        headers: {
+          Authorization: `Bearer ${this.jwtBearer}`,
+        },
+      })
+      .then((response) => {
+        if (response?.data) {
+          console.log('Data: ', response.data);
+          this.shipments = response.data;
+          this.rowData = response.data;
+
+          this.shipments.forEach((shipment) => {
+            const currentShipment: Shipment = {
+              Arrival: shipment.Arrival,
+              vessel: shipment.vessel,
+              Container_No: shipment.Container_No,
+              Departure: shipment.Departure,
+              Port_Of_Load: shipment.Port_Of_Load,
+              Status: shipment.Status,
+              shipping_line: shipment.shipping_line,
+              documents: shipment.documents,
+            } as Shipment;
+          });
+        }
+      })
+      .catch(error => {
+        console.log('An error occurred:', error.response);
+      });
   }
 }
